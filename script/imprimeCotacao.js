de52cc75c5a9ccb6c32d17e0301d63bb9a906663
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2023-11-14 13:50:52 -0300

    Fim do curso.

--- script/imprimeCotacao.js
+++ script/imprimeCotacao.js
@@ -1,10 +1,13 @@
-const lista = document.querySelector('[data-lista]')
-
 function imprimeCotacao(nome, valor) {
+    const lista = document.getElementById(nome)
     lista.innerHTML = '';
     for (let multiplicador = 1; multiplicador <= 1000; multiplicador *= 10) {
         const listaItem = document.createElement('li')
-        listaItem.innerHTML = `${multiplicador} ${nome}: R$${(valor * multiplicador).toFixed(2)}`
+        const plurais = {
+            'dolar': 'dolares',
+            'iene': 'ienes'
+        }
+        listaItem.innerHTML = `${multiplicador} ${multiplicador > 1 ? plurais[nome] : nome}: R$${(valor * multiplicador).toFixed(2)}`
         lista.appendChild(listaItem)
     }
 }
--- script/scripts.js
+++ script/scripts.js
@@ -12,7 +12,7 @@ const graficoParaDolar = new Chart(graficoDolar, {
             borderWidth: 1
         }]
     }
-});
+})
 
 function geraHorario() {
     let data = new Date()
@@ -36,4 +36,26 @@ workerDolar.addEventListener('message', event => {
     let valor = event.data.ask
     imprimeCotacao('dolar', valor)
     adicionarDados(graficoParaDolar, tempo, valor)
+})
+
+const graficoIene = document.getElementById('graficoIene')
+const graficoParaIene = new Chart(graficoIene, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Iene',
+            data: [],
+            borderWidth: 1
+        }]
+    }
+})
+
+let workerIene = new Worker('./script/workers/workerIene.js')
+workerIene.postMessage('iene')
+workerIene.addEventListener('message', event => {
+    let tempo = geraHorario()
+    let valor = event.data.ask
+    imprimeCotacao('iene', valor)
+    adicionarDados(graficoParaIene, tempo, valor)
 })
